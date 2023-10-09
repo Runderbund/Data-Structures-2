71301fe502dff70126f8676d356a04e4ee838bcb
unknown <sean.herbison@gmail.com>
2023-10-09 15:06:26 -0700

    Built skeleton for exercise.

---
+++ binary_node.py
@@ -0,0 +1,12 @@
+class BinaryNode:
+    def __init__(self):
+        self.data = None
+        self.left = None
+        self.right = None
+
+def insert_node():
+    pass
+
+def search_for_node():
+    pass
+
---
+++ linked_list.py
@@ -0,0 +1,15 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+def append_node():
+    pass
+
+def find_node():
+    pass
