5f1d8856cabaffa8fe98182188b535d6f02869b3
unknown <sean.herbison@gmail.com>
2023-10-10 17:26:26 -0700

    Added Binary Tree test to Main.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from linked_list import LinkedList
+from binary_node import BinarySearchTree
 
 class RunMain:
     def __init__(self) -> None:
         self.list = LinkedList()
+        self.tree = BinarySearchTree()
+
 
     def append_nodes(self):
         print("\nAdd a new node with the value of 5")
@@ -19,7 +22,26 @@ class RunMain:
         print(self.list.find_node(22))
 
 
+    def populate_tree(self):
+        print("\nInserting nodes into the binary tree")
+        self.tree.insert_node(50)
+        self.tree.insert_node(30)
+        self.tree.insert_node(70)
+        self.tree.insert_node(15)
+        self.tree.insert_node(75)
+
+    def search_tree(self):
+        print("\nSearching for a node with value 70 in the binary tree")
+        result = self.tree.search_for_node(70)
+        print(f"Node found: {result.data if result else None}")
+
+        print("\nSearching for a node with value 100 in the binary tree")
+        result = self.tree.search_for_node(100)
+        print(f"Node found: {result.data if result else None}")
+
 if __name__== '__main__':
     main = RunMain()
-    main.append_nodes ()
+    main.append_nodes()
     main.find_values()
+    main.populate_tree()
+    main.search_tree()
