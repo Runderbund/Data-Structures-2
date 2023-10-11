c7aa879b2af20167343e9029f3e18740db7ed4b9
unknown <sean.herbison@gmail.com>
2023-10-10 17:08:16 -0700

    Added find_node to LinkedList class.

--- linked_list.py
+++ linked_list.py
@@ -16,3 +16,11 @@ class LinkedList:
         else:
             self.tail.next = new_node
             self.tail = new_node
+
+    def find_node(self, data):
+        current_node = self.head
+        while current_node is not None:
+            if current_node.data == data:
+                return True
+            current_node = current_node.next
+        return False
